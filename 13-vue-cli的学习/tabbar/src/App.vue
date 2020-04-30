1202e7326217365881692f9904453c44e366d1d3
meiwen520gmx <1045418346@qq.com>
2020-04-30 14:29:42 +0800

    新添加小案例tabbar

---
+++ 13-vue-cli的学习/tabbar/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!-- 底部导航 -->
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img slot="item-icon" src="./assets/img/tabbar/index.png" alt="" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/index_active.png" alt="" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="blue">
+        <img slot="item-icon" src="./assets/img/tabbar/fenlei.png" alt="" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/fenlei_active.png" alt="" />
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="yellow">
+        <img slot="item-icon" src="./assets/img/tabbar/shopcar.png" alt="" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/shopcar_active.png" alt="" />
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="profile" activeColor="green">
+        <img slot="item-icon" src="./assets/img/tabbar/mine.png" alt="" />
+        <img slot="item-icon-active" src="./assets/img/tabbar/mine_active.png" alt="" />
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar";
+import TabBarItem from "./components/tabbar/TabBarItem";
+export default {
+  name: "App",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
+
+<style></style>
