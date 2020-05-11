c6fc8f885dc4cc3c763742bfb598770f472661d7
高敏雪 <1045418346@qq.com>
2020-05-11 21:59:07 +0800

    x

--- 13-vue-cli的学习/learnvuerouter/src/router/index.js
+++ 13-vue-cli的学习/learnvuerouter/src/router/index.js
@@ -1,94 +1,93 @@
-import Vue from 'vue'
+import Vue from "vue";
 //配置路由相关的信息
-import Router from 'vue-router'
+import Router from "vue-router";
 // import HelloWorld from '@/components/HelloWorld'
 // import Home from '@/components/Home'
 // import About from '@/components/About'
 // import User from '@/components/User'
 
 //通过Vue.use（插件），安装插件
-Vue.use(Router)
+Vue.use(Router);
 
 //路由懒加载
-const HelloWorld = () => import('../components/HelloWorld')
-const Home = () => import('../components/Home')
-const About = () => import('../components/About')
-const User = () => import('../components/User')
-const News = () => import('../components/HomeNews')
-const Message = () => import('../components/HomeMessage')
-const Profile = () => import('../components/Profile')
-
+const HelloWorld = () => import("../components/HelloWorld");
+const Home = () => import("../components/Home");
+const About = () => import("../components/About");
+const User = () => import("../components/User");
+const News = () => import("../components/HomeNews");
+const Message = () => import("../components/HomeMessage");
+const Profile = () => import("../components/Profile");
 
 //创建Router对象
 const router = new Router({
-  mode:'history',
+  mode: "history",
   // linkActiveClass: 'active',//修改router-link被激活的样式名称router-link-active，改为active的简便改法
   //配置路由和组件之间的应用关系
   routes: [
     {
-      path: '',
+      path: "",
       //redirect重定向
-      redirect: '/home'
+      redirect: "/home"
     },
     {
-      path: '/helloworld',
-      name: 'HelloWorld',
+      path: "/helloworld/:flag",
+      name: "HelloWorld",
       component: HelloWorld,
       meta: {
-        title: '你好页'
+        title: "你好页"
       }
     },
     {
-      path: '/home',
-      name: 'Home',
+      path: "/home",
+      name: "Home",
       component: Home,
       meta: {
-        title: '首页'
+        title: "首页"
       },
       children: [
         {
-          path: '',
+          path: "",
           // component: News
-          redirect: 'news'
+          redirect: "news"
         },
         {
-          path: 'news',
+          path: "news",
           component: News
         },
         {
-          path: 'message',
+          path: "message",
           component: Message
         }
       ]
     },
     {
-      path: '/about',
-      name: 'About',
+      path: "/about",
+      name: "About",
       component: About,
       meta: {
-        title: '关于'
+        title: "关于"
       }
     },
     {
-      path: '/user/:userId',
-      name: 'User',
+      path: "/user/:userId",
+      name: "User",
       component: User,
       meta: {
-        title: '用户'
+        title: "用户"
       }
     },
     {
-      path: '/profile',
-      name: 'Profile',
+      path: "/profile",
+      name: "Profile",
       component: Profile,
       meta: {
-        title: '档案'
+        title: "档案"
       }
     }
   ]
-})
+});
 
-export default router
+export default router;
 //export default 是将Router对象传入到vue实例
 
 // 第一步：导入路由对象，并且调用Vue.use(VueRouter)
@@ -97,14 +96,11 @@ export default router
 
 //前置守卫
 router.beforeEach((to, from, next) => {
-  console.log(to)
+  console.log(to);
   //设置每个页面的title
-  document.title = to.matched[0].meta.title
-  next()
-})
+  document.title = to.matched[0].meta.title;
+  next();
+});
 
 //后置钩子
-router.afterEach((to, from) => {
-
-})
-
+router.afterEach((to, from) => {});
