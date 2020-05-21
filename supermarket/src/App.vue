8788d8d1d883317ca4ef31bac13035e3811da051
meiwen520gmx <1045418346@qq.com>
2020-05-21 15:47:08 +0800

    项目模块划分

--- supermarket/src/App.vue
+++ supermarket/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
+<script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+export default {
+  components: {
+    MainTabBar,
+  },
+}
+</script>
 <style lang="less">
-@import "assets/css/common";
 </style>
---
+++ supermarket/src/views/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <div class="heihei">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar"
+  export default {
+    components: {
+      NavBar,
+    },
+  }
+</script>
+
+<style lang="less" scoped>
+.heihei{
+  height: 50px;
+}
+.home-nav{
+  background-color: @colorTint;
+}
+</style>
