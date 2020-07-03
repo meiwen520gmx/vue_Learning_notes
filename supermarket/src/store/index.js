0f012c946bdc46242ee56833cc83573ab05c1844
meiwen520gmx <1045418346@qq.com>
2020-07-03 14:11:24 +0800

    增加购物车功能有待完善

--- supermarket/src/store/index.js
+++ supermarket/src/store/index.js
@@ -9,19 +9,28 @@ export default new Vuex.Store({
   },
   mutations: {
     addCart(state, payload) {
-      if (state.cartList.length !== 0) {
-        state.cartList.forEach(item => {
-          if(item.iid === payload.iid){
-            item.count += 1;
-          }else{
-            payload.count = 1;
-            state.cartList.push(payload);
-          }
-        })
+      //方法一：
+      let oldProduct = state.cartList.find(item => item.iid === payload.iid);
+      if (oldProduct) {
+        oldProduct.count += 1;
       } else {
         payload.count = 1;
         state.cartList.push(payload);
       }
+      //方法二:
+      // if (state.cartList.length !== 0) {
+      //   state.cartList.forEach(item => {
+      //     if(item.iid === payload.iid){
+      //       item.count += 1;
+      //     }else{
+      //       payload.count = 1;
+      //       state.cartList.push(payload);
+      //     }
+      //   })
+      // } else {
+      //   payload.count = 1;
+      //   state.cartList.push(payload);
+      // }
     }
   },
   actions: {},
